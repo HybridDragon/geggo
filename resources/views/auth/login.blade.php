56ac5822049d431357adc3b252ed32e3a6047ef9
Aidhan Dossel <me@aidhan.net>
2015-04-09 07:53:56 +0800

    Fix form URLs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-	<form name="login" method="post" action="{{ action('AuthController@login') }}">
+	<form name="login" method="post" action="{{ secure_url('/auth/login') }}">
 		<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 		<div>
@@ -9,5 +9,8 @@
 			<input type="password" name="password" required placeholder="and password">
 			<input type="submit" value="log me in!">
 		</div>
+
+		<aside>
+		</aside>
 	</form>
 @stop
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-	<form name="signup" method="post" action="{{ action('AuthController@signup') }}">
+	<form name="signup" method="post" action="{{ secure_url('/auth/signup') }}">
 		<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 		<div>
@@ -14,11 +14,7 @@
 		</div>
 
 		<aside>
-			<p id="signup-tips"></p>
-			<p id="username-input-tips">Will be used to identify you, use something unique</p>
-			<p id="email-input-tips">So we can send you notifications (not spam)</p>
-			<p id="password-input-tips">Something that's hard for others to guess</p>
-			<p id="password-confirm-input-tips">Confirm your password so we don't get it wrong</p>
+			<p>test</p>
 		</aside>
 	</form>
 @stop
