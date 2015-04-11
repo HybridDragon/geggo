8cdecf91a489ac4abfd72af7b6b2222f37e9f453
Aidhan Dossel <me@aidhan.net>
2015-04-12 01:53:38 +0800

    Restructure forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,13 +4,22 @@
 	<form name="login" method="post" action="{{ secure_url('/auth/login') }}">
 		<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
+		<h4>Username</h4>
 		<div>
 			<input type="text" name="username" required placeholder="your username">
-			<input type="password" name="password" required placeholder="and password">
-			<input type="submit" value="log me in!">
+			<aside>
+				<p></p>
+			</aside>
+		</div> 
+
+		<h4>Password</h4>
+		<div>
+			<input type="password" name="password" required placeholder="your password">
+			<aside>
+				<p></p>
+			</aside>
 		</div>
 
-		<aside>
-		</aside>
+		<input type="submit" value="log me in!">
 	</form>
 @stop
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -4,17 +4,38 @@
 	<form name="signup" method="post" action="{{ secure_url('/auth/signup') }}">
 		<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
+		<h4>Username</h4>
 		<div>
 			<input type="text" name="username" required placeholder="a sweet username">
-			<input type="email" name="email" required placeholder="your awesome email">
-			<input type="password" name="password" required placeholder="a secure password">
-			<input type="password" name="password-confirm" required placeholder="your password again">
-
-			<input type="submit" value="sign me up!">
+			<aside>
+				<p>Your username should be something unique to you as it will be used to identify you, a nickname you use is generally the way to go.</p>
+			</aside>
 		</div>
 
-		<aside>
-			<p>test</p>
-		</aside>
+		<h4>Email address</h4>
+		<div>
+			<input type="email" name="email" required placeholder="your awesome email">
+			<aside>
+				<p>We'll need your email address so we can send you a few things such as password reset requests and notifications.</p>
+			</aside>
+		</div>
+
+		<h4>Password</h4>
+		<div>
+			<input type="password" name="password" required placeholder="a secure password">
+			<aside>
+				<p>A strong password is typically made up of a combination of letters, numbers and symbols. Try using a short, memorable phrase and replace some of the characters with numbers or symbols.</p>
+			</aside>
+		</div>
+
+		<h4>Password confirmation</h4>
+		<div>
+			<input type="password" name="password-confirm" required placeholder="your password again">
+			<aside>
+				<p>We're noting down your password again so that it's correct and we don't save an incorrect password.</p>
+			</aside>
+		</div>
+
+		<input type="submit" value="sign me up!">
 	</form>
 @stop
